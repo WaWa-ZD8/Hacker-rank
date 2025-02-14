a9fa4d0450820fbfc0d46d334c4b187e6938ef24
Wathma Herath <wathmaherath437@gmail.com>
2025-02-14 13:16:37 +0530

    Update nestedList.py

--- nestedList.py
+++ nestedList.py
@@ -10,8 +10,7 @@ if __name__ == '__main__':
     second_lowest_grade = sort_score[1]  
     second_lowest_student = sorted([name for name, score in students if score == second_lowest_grade])
         
-print(students)  
-print( sort_score)
+
 print (second_lowest_student)
 
 
